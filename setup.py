bc375961bd892be0039b0cd1be3cd9536b7494b6
Brendan Hall <bhall@keiserlab.org>
2025-03-03 09:42:08 -0800

    More dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     packages=find_packages(include=["usearch", "rad"]),
     install_requires=[
         "redis>=5.2.1",
+        "matplotlib>=3.5.0",
+        "numpy>=1.26.4",
+        "tqdm>=4.66.2"
     ],
     python_requires=">=3.11",
     include_package_data=True,
