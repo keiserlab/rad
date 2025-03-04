d8d8bd35bebae36e4b3a74baa13eb85751467646
Brendan Hall <bhall@keiserlab.org>
2025-03-03 16:05:48 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="rad",
     version="0.2.0",
-    packages=find_packages(include=["usearch", "rad"]),
+    packages=find_packages(),
     install_requires=[
         "redis>=5.2.1",
         "matplotlib>=3.5.0",
         "numpy>=1.26.4",
         "tqdm>=4.66.2",
-        "notebook>=6.5.0"
+        "notebook>=6.5.0",
+        "rdkit>=2024.09.5",
+        # TODO: Improve the way we install the usearch submodule 
+        f"usearch @ file://localhost/{os.getcwd()}/usearch/"
     ],
     python_requires=">=3.11",
     include_package_data=True,
